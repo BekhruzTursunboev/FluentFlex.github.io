299a6c44bd467ccb12e7a367aee95ec70999120a
Bekhruz Tursunboev <122160021+BekhruzTursunboev@users.noreply.github.com>
2024-01-11 22:16:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMaterials(level) {
+    document.getElementById('selected-level').innerText = 'Выбранный уровень: ' + level;
+    document.getElementById('materials').style.display = 'block';
+}
